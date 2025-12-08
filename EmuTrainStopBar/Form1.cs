f9ec10c685ae4abf8f87995edc1574c339236f54
nataniachan <nathaniellim0@gmail.com>
2025-12-09 03:42:29 +0800

    no connection error message

--- EmuTrainStopBar/Form1.cs
+++ EmuTrainStopBar/Form1.cs
@@ -413,8 +413,8 @@ namespace EmuTrainStopBar
                     // Usually happens when the PINE server is not active yet
                     labelGameName.Text = $"System standby ({PineMode})";
                     labelGameId.Text = "PINE communication error.";
-                    labelSettings.Text = "Check that it is enabled.";
-                    labelDistance.Text = "";
+                    labelSettings.Text = "Check that it is enabled,";
+                    labelDistance.Text = "and a game is running.";
 
                     RestoreStyle();
                     return;
